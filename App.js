b862aa4d92060a1c45654f43d41bd28ee2ec9449
ghousekhas <ghouse.khasim1@gmail.com>
2020-07-13 10:19:41 +0530

    First init

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import * as Notifications from 'expo-notifications';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}> Something's going on in here</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +21,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize: 30,
+    textAlign: "center",
+    transform: [
+      { translateX: -Dimensions.get('window').width*0.24},
+      { rotateY: '60deg'},
+      { perspective: 850 }
+    ],
+    color: '#0000FF'
+  }
 });
