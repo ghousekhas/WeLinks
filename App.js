83f52ecc835ef346729969432afebcfd313f038e
ghousekhas <ghouse.khasim1@gmail.com>
2020-07-13 11:21:15 +0530

    SplashScreen

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import * as Notifications from 'expo-notifications';
 
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as Notiifications from 'expo-notifications';
+import Introduction from './screens/Introduction'
 
-export default function App() {
+/*function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}*/
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Introduction" component={Introduction}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+/*export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -31,4 +55,4 @@ const styles = StyleSheet.create({
     ],
     color: '#0000FF'
   }
-});
+});*/
